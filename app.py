5a2f5afd15936120e0e984b47b5cffe701e91749
Sprocketer <90787028+Sprocketer@users.noreply.github.com>
2024-04-24 12:40:47 +0000

    V2

--- app.py
+++ app.py
@@ -2,15 +2,16 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=["GET", "POST"])
 def index():
-    username = request.args.get("username")
-    password = request.args.get("password")
-    print(username)
-    print(password)
-    if username == None:
+    if request.method == "GET":
         return render_template("index.html")
-    elif username == "admin" and password == "admin123":
-        return "<h1>Admin</h1>"
-    else:
-        return "<h1>User unrecognised.</h1>"
+    elif request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+        print(username)
+        print(password)
+        if username == "admin" and password == "admin123":
+            return "<h1>Admin</h1>"
+        else:
+            return "<h1>User unrecognised.</h1>"
